6b25b8e43706ba9f71263cc772951bd711d6ac9b
Pavan Kumar Pannala <pavankumar.pannala@outlook.com>
2019-09-09 13:35:35 +0530

    Changed

---
+++ AsyncAwaitChallenge/Controllers/EmployeeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AsyncAwaitChallenge.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DemoAPI.Controllers
+{
+           
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+        public EmployeeController(ILogger<EmployeeController> logger)
+        {
+            logger.LogInformation("Created");
+        }
+
+        [HttpGet]
+        [Route("api/employees/employee/{id}")]
+        public ActionResult<EmployeeData> GetByID(int id)
+        {
+            var employee = new EmployeeData()
+            {
+                ID = id,
+                FirstName = "Test",
+                LastName = "Test1",
+                DateOfBirth = DateTime.Now.AddYears(-30)
+            };
+
+            return Ok(employee);
+        }
+
+        [HttpGet]
+        [Route("api/getallemployees")]
+        public ActionResult<List<EmployeeData>> GetEmployees()
+        {
+            var employees = new List<EmployeeData>()
+            {
+                new EmployeeData()
+                {
+                    ID = 1,
+                    FirstName = "First Name",
+                    LastName = "Last Name",
+                    DateOfBirth = DateTime.Now.AddYears(-30)
+                },
+                new EmployeeData()
+                {
+                    ID = 2,
+                    FirstName = "First Name",
+                    LastName = "Last Name",
+                    DateOfBirth = DateTime.Now.AddYears(-37)
+                },
+                new EmployeeData()
+                {
+                    ID = 3,
+                    FirstName = "First Name",
+                    LastName = "Last Name",
+                    DateOfBirth = DateTime.Now.AddYears(-3)
+                },
+                 new EmployeeData()
+                {
+                    ID = 3,
+                    FirstName = "First Name",
+                    LastName = "Last Name",
+                    DateOfBirth = DateTime.Now.AddYears(-1)
+                }
+            };
+
+
+            return Ok(employees);
+        }
+    }
+}
+
